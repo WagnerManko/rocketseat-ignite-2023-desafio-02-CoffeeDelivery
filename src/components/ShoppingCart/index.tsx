582499d34d9903c204b7bf518412df9c28a1e1b5
Wagner Mengue <45788235+WagnerManko@users.noreply.github.com>
2023-03-10 19:07:04 -0300

    Coffee Cart layout

--- src/components/ShoppingCart/index.tsx
+++ src/components/ShoppingCart/index.tsx
@@ -1,5 +1,49 @@
+import { useContext, useReducer } from "react"
+import { CartContext } from "../../contexts/CartContext"
+
+import styles from './styles.module.scss'
+import { Minus, Plus, Trash } from "phosphor-react"
+
 export function ShoppingCart(){
+    const { cartItems } = useContext(CartContext)
+
+    console.log(cartItems)
+
     return (
-        <h1>Cart2</h1>
+        <>
+            {cartItems.map(item => (
+                <div key={item.id} className={styles.cartItem}>
+                    <div  className={styles.coffee}>
+                        <img src={`./coffees/coffee-${item.id}.png`} alt="" />
+
+                        <div className={styles.coffeeQuantity}>
+                            <h1>{item.name}</h1>
+                            <div>
+                                <div className={styles.addAndRemoveBtn}>
+                                    <button>
+                                        <Minus size={14} />
+                                    </button>
+                                    <span>1</span>
+                                    <button>
+                                        <Plus size={14} />
+                                    </button>
+                                </div>
+                                
+                                <button><Trash size={16} /> <p>Remover</p></button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <p>R$ {Number(item.cost * item.quantity!).toFixed(2)}</p>
+                </div>
+            ))}
+
+            <div className={styles.amount}>
+                <p>Total de itens <span>R$ 29,70</span></p>
+                <p>Entrega <span>R$ 3,50</span></p>
+                <p>Total <span>R$ 33,20</span></p>
+                <button><span>Confirmar Pedido</span></button>
+            </div>
+        </>
     )
 }
