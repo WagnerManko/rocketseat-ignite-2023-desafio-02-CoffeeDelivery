8e2ed16134fc7302f22985051cbff8e4592dafa5
Wagner Mengue <45788235+WagnerManko@users.noreply.github.com>
2023-03-24 16:59:52 -0300

    Form Delivery and Payment Created

--- src/components/Delivery/index.tsx
+++ src/components/Delivery/index.tsx
@@ -1,5 +1,46 @@
+import { MapPinLine } from 'phosphor-react'
+import styles from './styles.module.scss'
+import { useState } from "react";
+
 export function Delivery(){
+    const [complementoFilled, setComplementoFilled] = useState(false)
+
     return (
-        <h1>Delivery2</h1>
+        <>
+            <div className={styles.formTitle}>
+                <MapPinLine size={24} />
+                <div>
+                    <h1>Endereço de Entrega</h1>
+                    <h2>Informe o endereço onde deseja receber seu pedido</h2>
+                </div>
+            </div>
+
+            <div className={styles.divForm}>
+                <form onSubmit={() => {}}>
+                    <input type="text" className={styles.cep} placeholder="CEP" />
+                    
+                    <input type="text" placeholder="Rua" />
+
+                    <div>
+                        <input type="text" className={styles.number} placeholder="Número" />
+                        
+                        <div>
+                            <input type="text" placeholder="Complemento" onChange={(e) => {
+                                e.target.value.length != 0 ?
+                                setComplementoFilled(true) :
+                                setComplementoFilled(false)
+                            }}/>
+                            {!complementoFilled && <span>Opcional</span>}
+                        </div>
+                        
+                        <input type="text" className={styles.neighborhood} placeholder="Bairro" />
+
+                        <input type="text" className={styles.city} placeholder="Cidade" />
+
+                        <input type="text" className={styles.uf} placeholder="UF" />
+                    </div>
+                </form>
+            </div>
+        </>
     )
 }
--- src/components/Payment/index.tsx
+++ src/components/Payment/index.tsx
@@ -1,5 +1,45 @@
+import { useState } from "react"
+import { Bank, CreditCard, CurrencyDollar, Money } from "phosphor-react"
+import styles from './styles.module.scss'
+
 export function Payment(){
+    const [ choseMethod, setChoseMethod ] = useState('')
+
     return (
-        <h1>Payment2</h1>
+        <>
+            <div className={styles.paymentTitle}>
+                <CurrencyDollar size={24} />
+                <div>
+                    <h1>Pagamento</h1>
+                    <h2>O pagamento é feito na entrega. Escolha a forma que deseja pagar</h2>
+                </div>
+            </div>
+
+            <div className={styles.paymentMethods}>
+                <button
+                    onClick={() => setChoseMethod('credit')}
+                    className={choseMethod === 'credit' ? styles.active : ''}
+                >
+                    <CreditCard size={16} />
+                    <p>Cartão de crédito</p>
+                </button>
+                
+                <button
+                    onClick={() => setChoseMethod('debit')}
+                    className={choseMethod === 'debit' ? styles.active : ''}
+                >
+                    <Bank size={16} />
+                    <p>cartão de débito</p>
+                </button>
+                
+                <button
+                    onClick={() => setChoseMethod('money')}
+                    className={choseMethod === 'money' ? styles.active : ''}
+                >
+                    <Money size={16} />
+                    <p>dinheiro</p>
+                </button>
+            </div>
+        </>
     )
 }
